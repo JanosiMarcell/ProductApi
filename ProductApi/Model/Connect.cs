c5e0d6ea239ae409badb6dccb39269454194ac17
JanosiMarcell <janosim@kkszki.hu>
2024-09-30 10:21:49 +0200

    controller kesz

---
+++ ProductApi/Model/Connect.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace ProductApi.Model
+{
+    public class Connect
+    {
+        public MySqlConnection Connection;
+        public string MysqlConnectionString;
+
+
+        private string Host;
+        private string Database;
+        private string User;
+        private string Password;
+
+        public Connect()
+        {
+            Host= "localhost";
+            Database = "shop";
+            User= "root";
+            Password= "";
+        }
+    }
+}
---
+++ ProductApi/Model/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductApi.Model
+{
+    public class Product
+    {
+        public Guid id { get; set; }
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+        public DateTime CreatedTime { get; set; }
+    }
+}
